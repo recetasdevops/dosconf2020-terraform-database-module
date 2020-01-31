fbee5e5826ed8fec797c51398e197718c7fcc717
recetasdevops <44634828+recetasdevops@users.noreply.github.com>
2020-01-31 14:17:19 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 output "sql_database_name" {
-    value = azurerm_sql_database.DatabaseInstancePasionPorLosBits.name
+    value = azurerm_sql_database.sql_server_database.name
 }
 
 output "sql_server_domain_name" {
-  value = azurerm_sql_server.DatabaseServerPasionPorLosBits.fully_qualified_domain_name
+  value = azurerm_sql_server.sql_server.fully_qualified_domain_name
 }
 
 output "sql_user" {
-  value = azurerm_sql_server.DatabaseServerPasionPorLosBits.administrator_login
+  value = azurerm_sql_server.sql_server.administrator_login
 }
 
 output "sql_password" {
-  value = azurerm_sql_server.DatabaseServerPasionPorLosBits.administrator_login_password
+  value = azurerm_sql_server.sql_server.administrator_login_password
 }
 
 output "sql_connectionstring" {
-  value = "Server=tcp:${azurerm_sql_server.DatabaseServerPasionPorLosBits.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_sql_database.DatabaseInstancePasionPorLosBits.name};Persist Security Info=False;User ID=${azurerm_sql_server.DatabaseServerPasionPorLosBits.administrator_login};Password=${azurerm_sql_server.DatabaseServerPasionPorLosBits.administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  value = "Server=tcp:${azurerm_sql_server.sql_server.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_sql_database.sql_server_database.name};Persist Security Info=False;User ID=${azurerm_sql_server.sql_server.administrator_login};Password=${azurerm_sql_server.sql_server.administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
 }
